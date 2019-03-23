161ede0c93b073043b9396daa0797ce5cf13c6c5
MaafiHanene <32295248+MaafiHanene@users.noreply.github.com>
2019-03-23 11:53:16 +0100

    Update 0004_auto_20190304_1837.py

--- sayaradz/migrations/0004_auto_20190304_1837.py
+++ sayaradz/migrations/0004_auto_20190304_1837.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('sayaradz', '0002_auto_20190301_1629'),
+        ('sayaradz', '0001_initial'),
     ]
 
     operations = [
