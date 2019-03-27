99a00a0225026b7d22edaaa03f6937275698470e
MaafiHanene <32295248+MaafiHanene@users.noreply.github.com>
2019-03-27 15:02:31 +0100

    Update settings.py

--- sayaradz_api/settings.py
+++ sayaradz_api/settings.py
@@ -93,32 +93,21 @@ WSGI_APPLICATION = 'sayaradz_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+
+
 DATABASES = {
-
         'default': {
-
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
             'NAME': os.environ.get("DATABASE_NAME"),
-
             'USER': os.environ.get("DATABASE_USER"),
-
             'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
-
             'HOST': os.environ.get("DATABASE_HOST"),
-
             'PORT': os.environ.get("DATABASE_PORT"),
-
             'TEST': {
-
                 'NAME': 'sayaradz_test',
-
             },
-
-
-
         }
-
     }
 
 
