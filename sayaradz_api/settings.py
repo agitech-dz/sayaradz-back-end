55c03899684a93de7d18599e51d1185498faa8ba
Maafi Hanene <32295248+MaafiHanene@users.noreply.github.com>
2019-06-23 22:31:01 +0100

    Update settings.py

--- sayaradz_api/settings.py
+++ sayaradz_api/settings.py
@@ -106,6 +106,8 @@ DATABASES = {
             'NAME': os.environ.get("DATABASE_NAME"),
             'USER': os.environ.get("DATABASE_USER"),
             'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+            'HOST': os.environ.get("DATABASE_HOST"),
+            'PORT': os.environ.get("DATABASE_PORT"),
             'TEST': {
                 'NAME': 'sayaradz_test',
             },
